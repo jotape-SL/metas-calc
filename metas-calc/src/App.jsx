85f78e903bb8fee367135db0c8cb6be5da7ba340
João Pedro de Sousa Cruz <jotape-SL@users.noreply.github.com>
2025-12-11 09:41:37 -0300

    abstracao do calculo das metas

--- metas-calc/src/App.jsx
+++ metas-calc/src/App.jsx
@@ -1,77 +1,70 @@
 import { useState } from 'react'
 import './App.css'
+import { calcularMetas } from './utils/calcularMetas';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [metaIntegral, setMetaIntegral] = useState(0);
   const [valorVendidoNaSemana, setValorVendidoNaSemana] = useState(0);
   const [diasDaSemanaRestantes, setDiasDaSemanaRestantes] = useState(0);
-  const [resultado, setResultado] = useState(false);
-  const [meta85, setMeta85] = useState(0);
-  const [meta90, setMeta90] = useState(0);
-  const [meta95, setMeta95] = useState(0);
-  const [meta100, setMeta100] = useState(0);
-  const [meta105, setMeta105] = useState(0);
-  const [meta110, setMeta110] = useState(0);
+  const [metas, setMetas] = useState(null);
 
-  function calcularMetas() {
-    setMeta85((((Number(metaIntegral)*.85) - Number(valorVendidoNaSemana))/ Number(diasDaSemanaRestantes)));
-    setMeta90((((Number(metaIntegral)*.9) - Number(valorVendidoNaSemana))/ Number(diasDaSemanaRestantes)));
-    setMeta95((((Number(metaIntegral)*.95) - Number(valorVendidoNaSemana))/ Number(diasDaSemanaRestantes)));
-    setMeta100(((Number(metaIntegral) - Number(valorVendidoNaSemana))/ Number(diasDaSemanaRestantes)));
-    setMeta105((((Number(metaIntegral)*1.05) - Number(valorVendidoNaSemana))/ Number(diasDaSemanaRestantes)));
-    setMeta110((((Number(metaIntegral)*1.1) - Number(valorVendidoNaSemana))/ Number(diasDaSemanaRestantes)));
+  function handleCalcular() {
+    const resultado = calcularMetas(
+      metaIntegral,
+      valorVendidoNaSemana,
+      diasDaSemanaRestantes
+    );
+
+    setMetas(resultado);
   }
-
-  
 
   return (
     <>
       <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>Calculadora de metas simples versao1.0 beta alpha meta tester ultra mega blaster 2000 exterminator</h1>
+        <h1>Calculadora de metas simples versao1.0 beta alpha meta tester ultra mega blaster 2000 exterminator</h1>
 
-      <div>
-        <label>Valor de 100% da meta da semana:</label><br />
-        <input
-          type="number"
-          value={metaIntegral}
-          onChange={(e) => setMetaIntegral(e.target.value)}
-        />
+        <div>
+          <label>Valor de 100% da meta da semana:</label><br />
+          <input
+            type="number"
+            value={metaIntegral}
+            onChange={(e) => setMetaIntegral(e.target.value)}
+          />
+        </div>
+
+        <div style={{ marginTop: "10px" }}>
+          <label>Valor total de vendido na semana:</label><br />
+          <input
+            type="number"
+            value={valorVendidoNaSemana}
+            onChange={(e) => setValorVendidoNaSemana(e.target.value)}
+          />
+        </div>
+
+        <div style={{ marginTop: "10px" }}>
+          <label>Dias restantes ate o final dessa semana:</label><br />
+          <input
+            type="number"
+            value={diasDaSemanaRestantes}
+            onChange={(e) => setDiasDaSemanaRestantes(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button onClick={handleCalcular}>Calcular metas</button>
+        </div>
+
+        {metas !== null && (
+          <>
+            <h2>90% Considerado: {metas.meta85}</h2>
+            <h2>90% Cheio: {metas.meta90}</h2>
+            <h2>100% Considerado: {metas.meta95}</h2>
+            <h2>100% Cheio: {metas.meta100}</h2>
+            <h2>110% Considerado: {metas.meta105}</h2>
+            <h2>110% Cheio: {metas.meta110}</h2>
+          </>
+        )}
       </div>
-
-      <div style={{ marginTop: "10px" }}>
-        <label>Valor total de vendido na semana:</label><br />
-        <input
-          type="number"
-          value={valorVendidoNaSemana}
-          onChange={(e) => setValorVendidoNaSemana(e.target.value)}
-        />
-      </div>
-
-      <div style={{ marginTop: "10px" }}>
-        <label>Dias restantes ate o final dessa semana:</label><br />
-        <input
-          type="number"
-          value={diasDaSemanaRestantes}
-          onChange={(e) => setDiasDaSemanaRestantes(e.target.value)}
-        />
-      </div>
-
-      <div style={{ marginTop: "20px" }}>
-        <button onClick={calcularMetas}>Calcular metas</button>
-      </div>
-
-      {resultado !== null && (
-        <>
-        <h2 style={{ marginTop: "20px" }}>90% Considerado: {meta85}</h2>
-        <h2 style={{ marginTop: "20px" }}>90% Cheio: {meta90}</h2>
-        <h2 style={{ marginTop: "20px" }}>100% Considerado: {meta95}</h2>
-        <h2 style={{ marginTop: "20px" }}>100% Cheio: {meta100}</h2>
-        <h2 style={{ marginTop: "20px" }}>110% Considerado: {meta105}</h2>
-        <h2 style={{ marginTop: "20px" }}>110% Cheio: {meta110}</h2>
-        </>
-      )}
-    </div>
     </>
   )
 }
