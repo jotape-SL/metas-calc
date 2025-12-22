d29da9b4d768f161ed0f0eb2c09e508238b91ab0
João Pedro de Sousa Cruz <jotape-SL@users.noreply.github.com>
2025-12-21 23:52:21 -0300

    visual do layout impresso

--- metas-calc/src/components/ResultadoMetasPrintable.jsx
+++ metas-calc/src/components/ResultadoMetasPrintable.jsx
@@ -1,40 +1,59 @@
 import styled from 'styled-components';
 
 function ResultadoMetasPrintable({ metas }) {
-    return (
-        <GeneralContainer>
-            <ContainerMetas>
-                <h2>Valores de 90% da meta</h2>
-                <h3>Cheio: {metas.meta90}</h3>
-                {/* <h3>Considerado: {metas.meta85}</h3> */}
-            </ContainerMetas>
-            <ContainerMetas>
-                <h2>Valores de 100% da meta</h2>
-                <h3>Cheio: {metas.meta100}</h3>
-                {/* <h3>Considerado: {metas.meta95}</h3> */}
-            </ContainerMetas>
-            <ContainerMetas>
-                <h2>Valores de 110% da meta</h2>
-                <h3>Cheio: {metas.meta110}</h3>
-                {/* <h3>Considerado: {metas.meta105}</h3> */}
-            </ContainerMetas>
-        </GeneralContainer>
-    );
+  return (
+    <GeneralContainer>
+      <ContainerGeralMetas>
+        <ContainerMetas>
+          <h2>Considerado</h2>
+          <h3>R$ {metas.meta85}</h3>
+        </ContainerMetas>
+        <ContainerMetas>
+          <h2>Cheio</h2>
+          <h3>R$ {metas.meta90}</h3>
+        </ContainerMetas>
+      </ContainerGeralMetas>
+      {/* <ContainerMetas>
+        <h2>Valores de 100% da meta</h2>
+        <h3>Cheio: {metas.meta100}</h3>
+      </ContainerMetas>
+      <ContainerMetas>
+        <h2>Valores de 110% da meta</h2>
+        <h3>Cheio: {metas.meta110}</h3>
+      </ContainerMetas> */}
+    </GeneralContainer>
+  );
 }
 
 export default ResultadoMetasPrintable;
 
 const ContainerMetas = styled.section`
-  padding: 2rem;
+  font-size: .6rem;
+  width: 130px;
+  padding: .2rem .5rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   z-index: 0;
+  border: 1px solid #000;
 `;
 
+const ContainerGeralMetas = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 416px;
+  position: absolute;
+  @media print{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`
+
 const GeneralContainer = styled.div`
-  display: none;
+  /* display: none; */
 
   @media print {
     background-color: #fff;
